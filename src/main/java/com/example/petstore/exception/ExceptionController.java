5b06d42fea1786c776e72994408f009cd83c7780
ruksar2992 <67260081+ruksar2992@users.noreply.github.com>
2020-07-07 12:21:42 +0530

    ruksar2

--- src/main/java/com/example/petstore/exception/ExceptionController.java
+++ src/main/java/com/example/petstore/exception/ExceptionController.java
@@ -5,13 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+
 @ControllerAdvice
 public class ExceptionController {
 	
 
-	@ExceptionHandler
-	public ResponseEntity<String> exceptionHandler(InvalidCredentialsException invalidCredentialsException) {
-		return new ResponseEntity<String>(invalidCredentialsException.getMessage(), HttpStatus.NOT_FOUND);
-	}
+	@ExceptionHandler(value = InvalidCredentialsException.class)
+	public ResponseEntity<ErrorResponse> exceptionHandler(InvalidCredentialsException invalidCredentialsException) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatusCode(401);
+        errorResponse.setMessage(invalidCredentialsException.getMessage());
+        
+        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 
 }
